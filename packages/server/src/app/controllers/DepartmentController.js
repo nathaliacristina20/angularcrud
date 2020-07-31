dd9cccae040f0703f67a925032b52ffd8bef0042
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-07-31 11:27:10 -0300

    fix update and delete departments

--- packages/server/src/app/controllers/DepartmentController.js
+++ packages/server/src/app/controllers/DepartmentController.js
@@ -32,7 +32,7 @@ class DepartmentController {
   async update(req, res) {
     try {
       const { id } = req.params;
-      const department = await Department.findById({ id });
+      const department = await Department.findById({ _id: id });
 
       if (!department) {
         return res.status(400).json({
@@ -42,12 +42,13 @@ class DepartmentController {
         });
       }
 
-      dep.name = req.body.name;
-      await dep.save();
+      department.name = req.body.name;
+      await department.save();
 
       return res.json(department);
       
     } catch (err) {
+      console.log(err);
       return res.status(500).json({
         message: {
           errorMessage: "Internal Server Error",
@@ -59,7 +60,7 @@ class DepartmentController {
   async delete(req, res) {
     try {
       const { id } = req.params;
-      await Department.deleteOne({ id });
+      await Department.deleteOne({ _id: id });
       return res.status(204).send("");
     } catch (err) {
       return res.status(500).json({
