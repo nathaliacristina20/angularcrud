303304d3ea1806ecae12e6ba26000a86ba20c82c
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-07-31 14:23:00 -0300

    create service for products and call to get

--- packages/web/src/app/interfaces/product.ts
+++ packages/web/src/app/interfaces/product.ts
@@ -3,7 +3,7 @@ import { Department } from "./department";
 export interface Product {
   id: string;
   name: string;
-  department: Department[];
+  department: Department[] | Array<string>;
   stock: number;
   price: number;
 }
--- packages/web/src/app/services/product.service.ts
+++ packages/web/src/app/services/product.service.ts
@@ -1,9 +1,47 @@
+import { DepartmentService } from './department.service';
+import { Product } from './../interfaces/product';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, combineLatest } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  readonly url = 'http://localhost:3333/products';
+  private productsSubject$: BehaviorSubject<Product[]> = new BehaviorSubject<
+    Product[]
+  >(null);
 
-  constructor() { }
+  private loaded = false;
+
+  constructor(
+    private http: HttpClient,
+    private departmentService: DepartmentService
+  ) {}
+
+  get(): Observable<Product[]> {
+    if (!this.loaded) {
+      combineLatest(
+        this.http.get<Product[]>(this.url),
+        this.departmentService.get()
+      ).pipe(
+        map(([products, departments]) => {
+          for (let product of products) {
+            let arrayDepartments = product.department as string[];
+            product.department = arrayDepartments.map((id) =>
+              departments.find((dep) => dep._id === id)
+            );
+          }
+          return products;
+        }),
+        tap((products) => console.log(products))
+      );
+
+      this.http.get<Product[]>(this.url).subscribe(this.productsSubject$);
+      this.loaded = true;
+    }
+    return this.productsSubject$.asObservable();
+  }
 }
